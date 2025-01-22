22f56db77bcb39170d1c34917374e7cf05d3eab6
Gon Solo <gonsolo@gmail.com>
2025-01-22 08:38:51 +0100

    Borg.

--- src/main/scala/Borg.scala
+++ src/main/scala/Borg.scala
@@ -18,7 +18,9 @@ import org.chipsalliance.cde.config.{Parameters, Field, Config}
 import org.chipsalliance.diplomacy.lazymodule.{InModuleBody, LazyModule}
 
 case class BorgParams(
-  address: BigInt = 0x4000,
+  regAddress: BigInt = 0x4000,
+  dmaAddress: BigInt = 0x5000,
+  dmaBytes: BigInt = 8,
   width: Int = 32)
 
 case object BorgKey extends Field[Option[BorgParams]](None)
@@ -26,45 +28,37 @@ case object BorgKey extends Field[Option[BorgParams]](None)
 class BorgIO(val w: Int) extends Bundle {
   val clock = Input(Clock())
   val reset = Input(Bool())
-  val input_ready = Output(Bool())
-  val input_valid = Input(Bool())
-  val x = Input(UInt(w.W))
-  val y = Input(UInt(w.W))
-  val output_ready = Input(Bool())
-  val output_valid = Output(Bool())
-  val borg_result = Output(UInt(w.W))
+
+  val start = Input(Bool())
+
+  val done = Output(Bool())
+
+  //val input_ready = Output(Bool())
+  //val input_valid = Input(Bool())
+  //val x = Input(UInt(w.W))
+  //val y = Input(UInt(w.W))
+  //val output_ready = Input(Bool())
+  //val output_valid = Output(Bool())
+  //val borg_result = Output(UInt(w.W))
 }
 
-class BorgMMIOChiselModule(val w: Int) extends Module {
+class BorgDataLoader(val w: Int) extends Module {
+
   val io = IO(new BorgIO(w))
-  val s_idle :: s_run :: s_done :: Nil = Enum(3)
+  val s_idle :: s_run :: Nil = Enum(2)
+  val state = RegInit(s_idle)
 
-  val state         = RegInit(s_idle)
-  val tmp           = Reg(UInt(w.W))
-  val borg_result   = Reg(UInt(w.W))
-
-  io.input_ready := state === s_idle
-  io.output_valid := state === s_done
-  io.borg_result := borg_result
-
-  when (state === s_idle && io.input_valid) {
+  when (state === s_idle && io.start) {
     state := s_run
-  } .elsewhen (state === s_run && tmp === 0.U) {
-    state := s_done
-  } .elsewhen (state === s_done && io.output_ready) {
+  } .elsewhen (state === s_run && io.done) {
     state := s_idle
   }
 
-  when (state === s_idle && io.input_valid) {
-    borg_result := io.x
-    tmp := io.y
-  } .elsewhen (state === s_run) {
-    when (borg_result > tmp) {
-      borg_result := borg_result - tmp
-    } .otherwise {
-      tmp := tmp - borg_result
-    }
+  when (state === s_run) {
+    // TODO
   }
+
+  io.done := state === s_idle
 }
 
 class BorgTL(params: BorgParams, beatBytes: Int)(implicit p: Parameters)
@@ -72,47 +66,63 @@ class BorgTL(params: BorgParams, beatBytes: Int)(implicit p: Parameters)
 
   val device = new SimpleDevice("borg-device", Seq("borg,borg-1"))
   val registerNode = TLRegisterNode(
-    Seq(AddressSet(params.address, 4096-1)),
+    Seq(AddressSet(params.regAddress, 4096-1)),
     device,
     "reg/control",
     beatBytes=beatBytes)
   val clientNode = TLClientNode(Seq(TLMasterPortParameters.v1(Seq(TLMasterParameters.v1(
-    name = "dma-test",
+    name = "borg-dma-test",
     sourceId = IdRange(0, 1))))))
 
-  override lazy val module = new BorgImpl
+  override lazy val module = new BorgImpl(this)
 
-  class BorgImpl extends Impl {
+  class BorgImpl(outer: BorgTL) extends Impl {
+
+    // DMA
+    val (tl_out, edge) = outer.clientNode.out(0)
+
+    val (legal, get_bundle) = edge.Get(0.U, params.dmaAddress.U, log2Ceil(params.dmaBytes).U)
+
+    // Management
     withClockAndReset(clock, reset) {
-      val x = Reg(UInt(params.width.W))
-      val y = Wire(new DecoupledIO(UInt(params.width.W)))
-      val borg_result = Wire(new DecoupledIO(UInt(params.width.W)))
-      val status = Wire(UInt(2.W))
 
-      val impl_io = {
-        val impl = Module(new BorgMMIOChiselModule(params.width))
-        impl.io
+      // 0: idle or running
+      // 1: done
+      val status = Wire(UInt(1.W))
+
+      //val x = Reg(UInt(params.width.W))
+      // kick.valid is set on write
+      val kick = Wire(new DecoupledIO(Bool())) 
+      //val y = Wire(new DecoupledIO(UInt(params.width.W)))
+      //val borg_result = Wire(new DecoupledIO(UInt(params.width.W)))
+
+      val data_loader_io = {
+        val data_loader= Module(new BorgDataLoader(params.width))
+        data_loader.io
       }
 
-      impl_io.clock := clock
-      impl_io.reset := reset.asBool
+      data_loader_io.clock := clock
+      data_loader_io.reset := reset.asBool
 
-      impl_io.x := x
-      impl_io.y := y.bits
-      impl_io.input_valid := y.valid
-      y.ready := impl_io.input_ready
+      data_loader_io.start := kick.valid
 
-      borg_result.bits := impl_io.borg_result
-      borg_result.valid := impl_io.output_valid
-      impl_io.output_ready := borg_result.ready
+      //impl_io.x := x
+      //impl_io.y := y.bits
+      //impl_io.input_valid := y.valid
+      //y.ready := impl_io.input_ready
 
-      status := Cat(impl_io.input_ready, impl_io.output_valid)
+      //borg_result.bits := impl_io.borg_result
+      //borg_result.valid := impl_io.output_valid
+      //impl_io.output_ready := borg_result.ready
+
+      //status := Cat(impl_io.input_ready, impl_io.output_valid)
+      status := data_loader_io.done
 
       registerNode.regmap(
-        0x00 -> Seq(RegField.r(2, status)),
-        0x04 -> Seq(RegField.w(params.width, x)),
-        0x08 -> Seq(RegField.w(params.width, y)),
-        0x0C -> Seq(RegField.r(params.width, borg_result))
+        0x00 -> Seq(RegField.r(1, status)),
+        0x04 -> Seq(RegField.w(1, kick)),     // set on write
+        //0x08 -> Seq(RegField.w(params.width, y)),
+        //0x0C -> Seq(RegField.r(params.width, borg_result))
       )
     }
   }
