3f4b0f5731daf92688d1ecc83d32de0cf3067216
Gon Solo <gonsolo@gmail.com>
2025-04-24 18:13:57 +0200

    Step through soc.

--- src/test/scala/BorgTest.scala
+++ src/test/scala/BorgTest.scala
@@ -1,7 +1,7 @@
 import borg._
 import chisel3._
 import chisel3.simulator.EphemeralSimulator._
-import freechips.rocketchip.prci.{ClockSinkDomain, ClockGroupIdentityNode, ClockSinkParameters}
+import freechips.rocketchip.prci._
 import freechips.rocketchip.subsystem.{Attachable, InterruptBusWrapper, PeripheryBus, PeripheryBusParams, PBUS}
 import freechips.rocketchip.tilelink.TLFragmenter
 import org.chipsalliance.cde.config.Parameters
@@ -13,13 +13,17 @@ import Constants._
 
 import org.chipsalliance.cde.config.{Parameters, Field, Config}
 
-//class BlaIo extends Bundle {
-//  val bla = Output(Bool())
-//}
+import freechips.rocketchip.tilelink.TLBusWrapper
+
+class SocIo extends Bundle {
+  val x = Bool()
+}
 
 class SocModuleImp(_outer: Soc) extends LazyModuleImp(_outer) {
-  //val io = IO(new BlaIo)
-  //io.bla := true.B
+
+  val io = IO(new SocIo)
+  io.x := true.B
+
   val parameters = new PeripheryBusParams(8, 8)
   val pbus = LazyModule(new PeripheryBus(parameters, "pbus"))
   val borg = pbus { LazyModule(new Borg(pbus.beatBytes)(p)) }
@@ -27,62 +31,28 @@ class SocModuleImp(_outer: Soc) extends LazyModuleImp(_outer) {
   printf(cf"SocModuleImp step\n")
 }
 
-//trait HasBlaIo {
-//  def io: BlaIo
-//}
-
-class Soc()(implicit p: Parameters) extends LazyModule with Attachable {
+class Soc()(implicit p: Parameters) extends LazyModule {
   val allClockGroupsNode = ClockGroupIdentityNode()
-  val ibus = LazyModule(new InterruptBusWrapper)
   val busContextName = "subsystem"
-  override lazy val module = new SocModuleImp(this)
-
-  //val bla = IO(Output(Bool())).suggestName("bla")
-  //bla := DontCare
-
+  lazy val module = Module(new SocModuleImp(this))
   printf(cf"Soc step\n")
 }
 
 class TestSoc extends Module {
   val parameters = new WithBorg()
   val soc = LazyModule(new Soc()(parameters))
+  val io = IO(new SocIo)
+  io.x := soc.module.io.x
 
   printf(cf"TestSoc step\n")
 }
 
-class MyBundle extends Bundle {
-  val x = Bool()
-}
-
-class MyLazyModuleImp(outer: MyLazyModule) extends LazyModuleImp(outer) {
-  val io = IO(new MyBundle())
-  io.x := true.B
-}
-
-class MyLazyModule()(implicit p: Parameters) extends LazyModule {
-  lazy val module = Module(new MyLazyModuleImp(this))
-}
-
-case object MyKey extends Field[Option[MyConfig]](None)
-case class My()
-
-class MyConfig() extends Config((site, here, up) => {
-  case MyKey => Some(My())
-})
-
-class MyModule extends Module {
-  val parameters = new MyConfig()
-  val myLazyModule = LazyModule(new MyLazyModule()(parameters))
-  val io = IO(new MyBundle())
-  io.x := myLazyModule.module.io.x
-}
-
-class MyTest extends AnyFlatSpec {
-
-  behavior of "My"
+class SocTest extends AnyFlatSpec {
+  behavior of "Soc"
   it should "instantiate" in {
-    simulate(new MyModule()) { m =>
-      m.io.x.expect(true.B)
+    simulate(new TestSoc()) { soc =>
+      soc.clock.step()
+      soc.io.x.expect(true.B)
     }
   }
 }
